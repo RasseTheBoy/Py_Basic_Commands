b50b38468e9da906bd69de9a6cbdeff78f792b9f
Rasse <rassemichael@gmail.com>
2022-12-09 05:36:09 +0200

    Updated __init__.py
    Added from .basic_commands import func_timer

--- py_basic_commands/__init__.py
+++ py_basic_commands/__init__.py
@@ -1,4 +1,6 @@
 from .basic_commands import try_traceback
+from .basic_commands import func_timer
+
 from .basic_commands import fprint
 from .basic_commands import enter_to_continue
 from .basic_commands import finput
