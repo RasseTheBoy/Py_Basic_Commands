d01b987c48db8b5194f56cc5f1979e9bf6ed0458
Rasse <rassemichael@gmail.com>
2023-01-13 23:40:37 +0200

    v0.1.51

--- py_basic_commands/basic_commands.py
+++ py_basic_commands/basic_commands.py
@@ -10,6 +10,7 @@ class FunctionTimer:
     def __init__(self) -> None:
         self.do_print:bool = True
         self.ret_time:bool = False
+        self.skip_intro:bool = True
         self.skip_inputs:bool = True
 
     def config(self, **kwargs):
@@ -20,11 +21,12 @@ class FunctionTimer:
         if 'skip_inputs' in kwargs:
             self.skip_inputs = kwargs['skip_inputs']
 
-    def _func_timer(self, do_print:bool=True, ret_time:bool=False, skip_inputs:bool=True):
+    def _func_timer(self, do_print:bool=True, ret_time:bool=False, skip_intro:bool=True, skip_inputs:bool=True):
         def timer(func):
             @wraps(func)
             def wrapper(*args, **kwargs):
-                fprint(f'Function timer started: {func.__name__}', do_print=do_print)
+                fprint(f'Function timer started: {self._get_func_name(func)}', do_print=not skip_intro and do_print)
+                
                 time_start = perf_counter()
                 ret_val = func(*args, **kwargs)
                 time_delta = perf_counter() - time_start
@@ -38,7 +40,8 @@ class FunctionTimer:
         return timer
     
     def __call__(self, func, *args, **kwargs):
-        fprint(f'Function timer started: {func.__name__}', do_print=self.do_print)
+        fprint(f'Function timer started: {self._get_func_name(func)}', do_print=not self.skip_intro and self.do_print)
+        
         time_start = perf_counter()
         ret_val = func(*args, **kwargs)
         time_delta = perf_counter() - time_start
@@ -49,16 +52,30 @@ class FunctionTimer:
             return ret_val, time_delta
         return ret_val
 
+    def _get_func_name(self, func):
+        try:
+            return func.__name__
+        except:
+            try:
+                return func.__class__.__name__
+            except Exception:
+                return ''
+        
+
     def _print_time(self, func, time_delta:float, skip_inputs:bool, do_print:bool, *args, **kwargs):
         if not do_print:
             return
 
-        time_delta = round(time_delta, 4)
+        func_name = self._get_func_name(func)
+        if round(time_delta, 4) == 0.0:
+            time_delta = f'0.0000...'
+        else:
+            time_delta = round(time_delta, 4)
 
         if skip_inputs:
-            fprint(f'Function {func.__name__} Took {time_delta} seconds to run')
+            fprint(f'Function {func_name} Took {time_delta} seconds to run')
         else:
-            fprint(f'Function {func.__name__} {args}{kwargs} Took {time_delta} seconds to run')
+            fprint(f'Function {func_name} {args}{kwargs} Took {time_delta} seconds to run')
 
 _func_timer = FunctionTimer()._func_timer
 func_timer = FunctionTimer()
