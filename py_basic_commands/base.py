a8882757ecd90ec44c067b1036c19d897d2fcea5
Rasse <rassemichael@gmail.com>
2024-02-04 02:11:24 +0200

    Update base.py

--- py_basic_commands/base.py
+++ py_basic_commands/base.py
@@ -1,6 +1,6 @@
+from py_basic_commands.file_dir_scripts import get_src_path
+
 # Only used for inheritance
-
-
 class Base:
     """Base class for all classes"""
     def __init__(self, do_print=True) -> None:
@@ -26,6 +26,7 @@ class Base:
                 setattr(self, key, value)
 
 
+# Only used for inheritance
 class EditorBase(Base):
     """Base class for all file editors"""
     def __init__(self, file_path:str, do_print=True) -> None:
@@ -40,6 +41,37 @@ class EditorBase(Base):
         super().__init__(do_print)
 
         self.file_path = file_path
+
+
+    def get_file_path(self) -> str:
+        """Get the file path
+        
+        Returns
+        -------
+        str
+            The file path"""
+        return self.file_path
+
+
+    def get_file_name(self) -> str:
+        """Get the file name
+        
+        Returns
+        -------
+        str
+            The file name"""
+        return get_src_path(self.file_path, ret_val='f')
+    
+
+    def get_dir_path(self) -> str:
+        """Get the directory path
+        
+        Returns
+        -------
+        str
+            The directory path"""
+        return get_src_path(self.file_path, ret_val='d')
+
 
     def __enter__(self):
         """Enter the context manager"""
